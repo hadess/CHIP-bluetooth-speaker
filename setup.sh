6012cb5e4499f18c57ac76d5f229af135d10779f
sunmorgus <sunmorgus@gmail.com>
2017-04-22 09:26:01 -0400

    Added run as root check

--- setup.sh
+++ setup.sh
@@ -8,6 +8,12 @@
 NAME=${NAME:-CHIP Bluetooth speaker}
 DEFAULT_PIN=${DEFAULT_PIN:-0000}
 
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 # Exit on failure
 set -e
 # Debug
